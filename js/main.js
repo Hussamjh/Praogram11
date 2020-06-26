cdc3c9cfe00b44642933d1a567148d86a4a8c4e7
Husam Haidarah <haidarhu@sheridan.ca>
2020-06-26 11:28:05 -0400

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-var count = 0;
-
+var count = localStorage.getItem('count') ;
+if(count == null || count == undefined) {
+    count = 0;
+}
 
 
 
 function add() {
     count++;
     document.getElementById('count').innerHTML = ''+count;
+    localStorage.setItem('count', count);
 }
 
 function remove() {
@@ -13,10 +16,14 @@ function remove() {
         count--;
     }
     document.getElementById('count').innerHTML = ''+count;
+    localStorage.setItem('count', count);
+
 }
 
 function reset() {
     count = 0;
     document.getElementById('count').innerHTML = ''+count;
+    localStorage.setItem('count', count);
+
 
 }
